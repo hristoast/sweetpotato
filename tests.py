02863b19daa8aa3036cb57a02494aa3a3b15af3d
Hristos N. Triantafillou <hristos@bhgdo.com>
2015-01-07 22:39:21 -0600

    Cleanted tests and added a few

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@
 # TODO: run when already running
 # TODO: stop when already stopped
 # TODO: webui when already webui-ing
+import os
 import unittest
 import sweetpotato
 
@@ -27,42 +28,7 @@ mem_format: {4}
 server_dir: {5}
 """.format(TEST_WORLD_NAME, TEST_MEM_MAX, TEST_MEM_MIN,
            TEST_BACKUP_DIR, TEST_MEM_FORMAT, TEST_SERVER_DIR)
-
-
-class SweetpotatoTests(unittest.TestCase):
-
-    def setUp(self):
-        # Write the test conf file
-        with open(TEST_CONF_FILE, 'w') as c:
-            for l in TEST_CONF.strip().splitlines():
-                c.write(l)
-                c.write('\n')
-            c.close()
-
-        # Install a server to test with
-        sweetpotato.arg_parse([
-            '--conf', '{}'.format(TEST_CONF_FILE),
-            '--create',
-            '--quiet'
-        ])
-
-    def test_as_conf_file(self):
-        config = sweetpotato.SweetpotatoConfig()
-        config.backup_dir = TEST_BACKUP_DIR
-        config.mem_format = TEST_MEM_FORMAT
-        config.mem_max = TEST_MEM_MAX
-        config.mem_min = TEST_MEM_MIN
-        config.server_dir = TEST_SERVER_DIR
-        self.assertIn(TEST_SERVER_DIR, config.as_conf_file)
-
-    def test_as_serverproperties(self):
-        config = sweetpotato.SweetpotatoConfig()
-        config.backup_dir = TEST_BACKUP_DIR
-        config.mem_format = TEST_MEM_FORMAT
-        config.mem_max = TEST_MEM_MAX
-        config.mem_min = TEST_MEM_MIN
-        config.server_dir = TEST_SERVER_DIR
-        server_properties = """generator-settings=
+TEST_SERVER_PROPERTIES = """generator-settings=
 op-permission-level=4
 allow-nether=true
 resource-pack-hash=
@@ -98,16 +64,7 @@ generate-structures=true
 view-distance=10
 motd=Welcome to SweetpotatoWorld!
         """
-        self.assertEqual(config.as_serverproperties, server_properties)
-
-    def test_json(self):  # TODO: test if forge and if not forge
-        config = sweetpotato.SweetpotatoConfig()
-        config.backup_dir = TEST_BACKUP_DIR
-        config.mem_format = TEST_MEM_FORMAT
-        config.mem_max = TEST_MEM_MAX
-        config.mem_min = TEST_MEM_MIN
-        config.server_dir = TEST_SERVER_DIR
-        json = """{
+JSON = """{
     "backup_dir": "/tmp/_sp_test_backup",
     "compression": "gz",
     "conf_file": null,
@@ -123,7 +80,65 @@ motd=Welcome to SweetpotatoWorld!
     "webui_port": 8080,
     "world_name": "SweetpotatoWorld"
 }"""
-        self.assertEqual(config.as_json, json)
+
+
+class SweetpotatoTests(unittest.TestCase):
+
+    def setUp(self):
+        self.config = sweetpotato.SweetpotatoConfig()
+        self.config.backup_dir = TEST_BACKUP_DIR
+        self.config.mem_format = TEST_MEM_FORMAT
+        self.config.mem_max = TEST_MEM_MAX
+        self.config.mem_min = TEST_MEM_MIN
+        self.config.server_dir = TEST_SERVER_DIR
+
+        # Write the test conf file
+        with open(TEST_CONF_FILE, 'w') as c:
+            for l in TEST_CONF.strip().splitlines():
+                c.write(l)
+                c.write('\n')
+            c.close()
+
+        # Install a server to test with
+        sweetpotato.arg_parse([
+            '--conf', '{}'.format(TEST_CONF_FILE),
+            '--create',
+            '--quiet'
+        ])
+
+    def test_as_conf_file(self):
+        self.assertIn(TEST_SERVER_DIR, self.config.as_conf_file)
+
+    def test_json(self):  # TODO: test if forge and if not forge
+        self.assertEqual(self.config.as_json, JSON)
+
+    def test_as_serverproperties(self):
+        self.assertEqual(self.config.as_serverproperties, TEST_SERVER_PROPERTIES)
+
+    def test_agree_to_eula(self):
+        eula_txt = os.path.join(TEST_SERVER_DIR, 'eula.txt')
+        with open(eula_txt, 'r') as et:
+            eula_txt_text = et.readlines()
+            et.close()
+        agreed = 'eula=true' in eula_txt_text[0]
+        self.assertTrue(agreed)
+
+    def test_dependency_check(self):
+        check = sweetpotato.dependency_check(('nothing', None))
+        self.assertRaises(sweetpotato.MissingExeError, check)
+
+    @unittest.expectedFailure
+    def test_get_fake_exe_path(self):
+        fake = sweetpotato.get_exe_path('FartBomb.EXE')
+        self.assertTrue(fake)
+
+    def test_get_real_exe_path(self):
+        ls = sweetpotato.get_exe_path('ls')
+        self.assertEqual(ls, '/bin/ls')
+
+    def test_get_jar(self):
+        jar = sweetpotato.get_jar(self.config)[0]
+        self.assertEqual(jar, 'minecraft_server.{}.jar'.format(sweetpotato.__mcversion__))
 
     def test_stop_when_stopped(self):
         self.assertRaises(
